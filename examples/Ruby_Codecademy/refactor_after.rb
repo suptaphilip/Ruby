62d92c4884a578ea631bf2aaf404727927e28815
KellyChan <kwailamchan@hotmail.com>
2014-01-03 15:07:10 +0800

    Create refactor_after.rb

---
+++ examples/Ruby_Codecademy/refactor_after.rb
@@ -0,0 +1,17 @@
+$VERBOSE = nil    # We'll explain this at the end of the lesson.
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+
+  puts "n must be an integer." unless n.is_a? Integer 
+  puts "n must be greater than 0." if n <= 0
+  
+  prime_array ||= []
+  prime = Prime.new
+  (n).times{prime_array.push(prime.next)}
+  
+  return prime_array
+  
+end
+
+first_n_primes(10)
